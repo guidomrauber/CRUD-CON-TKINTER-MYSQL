5a2bd9d8fef5984ca5c0afee55b770509e9d26dc
guidomrauber <49770318+guidomrauber@users.noreply.github.com>
2022-11-11 21:49:46 -0300

    terminado tutorial

--- ventana.py
+++ ventana.py
@@ -1,14 +1,14 @@
-from io import BufferedIOBase
+
 from tkinter import *
 from tkinter import ttk
 from countries import *
+from tkinter import messagebox
 
-    
 
 class Ventana(Frame):
     
     paises = Countries()
-    
+        
     def __init__(self, master=None):
         super().__init__(master,width=680, height=260)
         self.master = master
@@ -17,8 +17,9 @@ class Ventana(Frame):
         self.llenaDatos()
         self.habilitarCajas("disabled")  
         self.habilitarBtnOper("normal")
-        self.habilitarBtnGuardar("disabled")
-            
+        self.habilitarBtnGuardar("disabled")  
+        self.id=-1      
+                   
     def habilitarCajas(self,estado):
         self.txtISO3.configure(state=estado)
         self.txtCapital.configure(state=estado)
@@ -43,13 +44,15 @@ class Ventana(Frame):
     def limpiaGrid(self):
         for item in self.grid.get_children():
             self.grid.delete(item)
-        
-        
+                
     def llenaDatos(self):
         datos = self.paises.consulta_paises()        
         for row in datos:            
             self.grid.insert("",END,text=row[0], values=(row[1],row[2], row[3],row[4]))
-
+        
+        if len(self.grid.get_children()) > 0:
+            self.grid.selection_set( self.grid.get_children()[0] )
+            
     def fNuevo(self):         
         self.habilitarCajas("normal")  
         self.habilitarBtnOper("disabled")
@@ -57,26 +60,64 @@ class Ventana(Frame):
         self.limpiarCajas()        
         self.txtISO3.focus()
     
-    def fGuardar(self):        
-        self.paises.inserta_pais(self.txtISO3.get(),self.txtName.get(),self.txtCapital.get(),self.txtCurrency.get())
+    def fGuardar(self): 
+        if self.id ==-1:       
+            self.paises.inserta_pais(self.txtISO3.get(),self.txtName.get(),self.txtCapital.get(),self.txtCurrency.get())            
+            messagebox.showinfo("Insertar", 'Elemento insertado correctamente.')
+        else:
+            self.paises.modifica_pais(self.id,self.txtISO3.get(),self.txtName.get(),self.txtCapital.get(),self.txtCurrency.get())
+            messagebox.showinfo("Modificar", 'Elemento modificado correctamente.')
+            self.id = -1            
         self.limpiaGrid()
-        self.llenaDatos()  
+        self.llenaDatos() 
+        self.limpiarCajas() 
         self.habilitarBtnGuardar("disabled")      
         self.habilitarBtnOper("normal")
         self.habilitarCajas("disabled")
-            
-        
-        
+                    
     def fModificar(self):        
-        pass
-    
+        selected = self.grid.focus()                               
+        clave = self.grid.item(selected,'text')        
+        if clave == '':
+            messagebox.showwarning("Modificar", 'Debes seleccionar un elemento.')            
+        else:            
+            self.id= clave  
+            self.habilitarCajas("normal")                         
+            valores = self.grid.item(selected,'values')
+            self.limpiarCajas()            
+            self.txtISO3.insert(0,valores[0])
+            self.txtName.insert(0,valores[1])
+            self.txtCapital.insert(0,valores[2])
+            self.txtCurrency.insert(0,valores[3])            
+            self.habilitarBtnOper("disabled")
+            self.habilitarBtnGuardar("normal")
+            self.txtISO3.focus()
+                                        
     def fEliminar(self):
-        pass
-    
-    
-
+        selected = self.grid.focus()                               
+        clave = self.grid.item(selected,'text')        
+        if clave == '':
+            messagebox.showwarning("Eliminar", 'Debes seleccionar un elemento.')            
+        else:                           
+            valores = self.grid.item(selected,'values')
+            data = str(clave) + ", " + valores[0] + ", " + valores[1]
+            r = messagebox.askquestion("Eliminar", "Deseas eliminar el registro seleccionado?\n" + data)            
+            if r == messagebox.YES:
+                n = self.paises.elimina_pais(clave)
+                if n == 1:
+                    messagebox.showinfo("Eliminar", 'Elemento eliminado correctamente.')
+                    self.limpiaGrid()
+                    self.llenaDatos()
+                else:
+                    messagebox.showwarning("Eliminar", 'No fue posible eliminar el elemento.')
+                            
     def fCancelar(self):
-        pass
+        r = messagebox.askquestion("Calcelar", "Esta seguro que desea cancelar la operación actual")
+        if r == messagebox.YES:
+            self.limpiarCajas() 
+            self.habilitarBtnGuardar("disabled")      
+            self.habilitarBtnOper("normal")
+            self.habilitarCajas("disabled")
 
     def create_widgets(self):
         frame1 = Frame(self, bg="#bfdaff")
@@ -91,6 +132,7 @@ class Ventana(Frame):
         frame2.place(x=95,y=0,width=150, height=259)                        
         lbl1 = Label(frame2,text="ISO3: ")
         lbl1.place(x=3,y=5)        
+        #self.txtISO3=Entry(frame2,textvariable = self.ISO3)
         self.txtISO3=Entry(frame2)
         self.txtISO3.place(x=3,y=25,width=50, height=20)                
         lbl2 = Label(frame2,text="Country Name: ")
@@ -108,10 +150,12 @@ class Ventana(Frame):
         self.btnGuardar=Button(frame2,text="Guardar", command=self.fGuardar, bg="green", fg="white")
         self.btnGuardar.place(x=10,y=210,width=60, height=30)
         self.btnCancelar=Button(frame2,text="Cancelar", command=self.fCancelar, bg="red", fg="white")
-        self.btnCancelar.place(x=80,y=210,width=60, height=30)        
-        self.grid = ttk.Treeview(self, columns=("col1","col2","col3","col4"))        
-        self.grid.column("#0",width=50)
-        self.grid.column("col1",width=60, anchor=CENTER)
+        self.btnCancelar.place(x=80,y=210,width=60, height=30)         
+        frame3 = Frame(self,bg="yellow" )
+        frame3.place(x=247,y=0,width=420, height=259)                      
+        self.grid = ttk.Treeview(frame3, columns=("col1","col2","col3","col4"))        
+        self.grid.column("#0",width=60)
+        self.grid.column("col1",width=70, anchor=CENTER)
         self.grid.column("col2",width=90, anchor=CENTER)
         self.grid.column("col3",width=90, anchor=CENTER)
         self.grid.column("col4",width=90, anchor=CENTER)        
@@ -119,11 +163,10 @@ class Ventana(Frame):
         self.grid.heading("col1", text="ISO3", anchor=CENTER)
         self.grid.heading("col2", text="Country Name", anchor=CENTER)
         self.grid.heading("col3", text="Capital", anchor=CENTER)
-        self.grid.heading("col4", text="Currency Code", anchor=CENTER)        
-        self.grid.place(x=247,y=0,width=420, height=259)
-        
-        
-        
-        
-        
-        
+        self.grid.heading("col4", text="Currency Code", anchor=CENTER)                
+        self.grid.pack(side=LEFT,fill = Y)        
+        sb = Scrollbar(frame3, orient=VERTICAL)
+        sb.pack(side=RIGHT, fill = Y)
+        self.grid.config(yscrollcommand=sb.set)
+        sb.config(command=self.grid.yview)
+        self.grid['selectmode']='browse'        
